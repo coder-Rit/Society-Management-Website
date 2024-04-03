8b758c45c3fc90e18d12c3af0d2f38fea46b00d1
Rohan Ramchandra Sawant <142981096+Rohansawant734@users.noreply.github.com>
2024-04-03 14:19:04 +0530

    checking

--- user_list.js
+++ user_list.js
@@ -4,17 +4,19 @@ const btnClick = document.querySelector('.status');
 let changeStatus = function () {
     if(isClicked){
         btnClick.addEventListener('click', () => {
-            btnClick.innerText = "Inactive";
-            btnClick.style.backgroundColor = "rgba(240, 199, 185, 0.671)"
-            btnClick.style.color = "red";
+            btnClick.innerText = "Member";
         });
         isClicked = false;
     }
+    else if(isClicked){
+        btnClick.addEventListener('click', () => {
+            btnClick.innerText = "Tenant";
+        });
+        isClicked = false;  
+    }
     else{
         btnClick.addEventListener('click', () => {
-            btnClick.innerText = "Active";
-            btnClick.style.backgroundColor = " rgba(185, 240, 196, 0.671)"
-            btnClick.style.color = "green";
+            btnClick.innerText = "User";
         });
         isClicked = true;
     }
