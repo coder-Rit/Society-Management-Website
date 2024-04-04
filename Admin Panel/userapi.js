6ae005bb1fa8bf93018704df69aa9fa7a53d301e
Rohan Ramchandra Sawant <142981096+Rohansawant734@users.noreply.github.com>
2024-04-04 15:45:50 +0530

    Dashboard and api integrations

---
+++ Admin Panel/userapi.js
@@ -0,0 +1,51 @@
+async function getUser() {
+
+    const response = await fetch("https://socity-server-fourise.onrender.com/api/v1/getmembers", {
+        method: "GET"
+    })
+
+    if (response.ok === false) {
+        console.log("error");
+        return
+    }
+
+    const data = await response.json()
+
+    const users = data.users
+
+    let fullTbody = ""
+
+    for (let index = 0; index < users.length; index++) {
+        console.log(data)
+        const element = users[index];
+
+        const tempBody =
+
+            `
+            <tr>
+            <td>${index+1}</td>
+            <td>${element.name}</td>
+            <td>${element.phoneNumber}</td>
+            <td>${element.email}</td>
+            <td>${element.flatNumber}</td>
+            <td>${element.wing}</td>
+            <td class="stats">
+            ${element.userType}
+            </td>
+            <td class="operation">
+                <button><img src="/Feature images/delete_icon.png" alt=""></button>
+            </td>
+        </tr> 
+            `
+
+        fullTbody += tempBody
+
+    }
+
+    console.log(fullTbody);
+
+    document.getElementById("tbody").innerHTML = fullTbody
+}
+
+
+getUser()
--- login.js
+++ login.js
@@ -9,3 +9,59 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+
+async function signUp() {
+	
+	const name = document.getElementById("name").value
+	const phone = document.getElementById("phone").value
+	const email = document.getElementById("email").value
+	const password = document.getElementById("password").value
+	const flat = document.getElementById("flat").value
+	const wing = document.getElementById("wing").value
+	const userType = document.getElementById("userType").value
+
+
+	const formData = {
+		"name": name,
+		"phoneNumber": phone,
+		"email": email,
+		"password": password,
+		"flatNumber": flat,
+		"wing": wing,
+		"userType": userType
+	}
+
+	console.log("1");
+
+
+	try {
+
+
+		const response = await fetch("https://socity-server-fourise.onrender.com/api/v1/signup", {
+			method: "POST",
+			body: JSON.stringify(formData)
+
+		})
+
+		if (response.ok === false) {
+			console.log("error");
+			return
+	
+		}
+	
+
+	
+		const data = await response.json()
+	
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+	console.log("2");
+	
+
+
+}
+
